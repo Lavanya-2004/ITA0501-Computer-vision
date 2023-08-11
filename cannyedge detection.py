dfbdc7dcbb3a869606b385b5aa18bf6176782e15
Lavanya-2004 <113500289+Lavanya-2004@users.noreply.github.com>
2023-08-11 09:29:14 +0530

    Create cannyedge detection.py

---
+++ cannyedge detection.py
@@ -0,0 +1,10 @@
+import cv2
+img=cv2.imread(r"C:\Users\user\Pictures\Screenshots\PRA_files\img.jpg")
+cv2.imshow('original',img)
+cv2.waitKey(0)
+img_Gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img_Blur=cv2.GaussianBlur(img_Gray,(3,3),0)
+edges=cv2.Canny(image=img_Blur,threshold1=100,threshold2=200)
+cv2.imshow('Canny Edge Detection',edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
